ecb76837cb8d14bbc42aeb2e12e3844d2e0d6e77
niomava <110886249+niomava@users.noreply.github.com>
2022-08-19 20:01:34 +0600

    Update Calculator v.1.0 .py

--- Calculator v.1.0 .py
+++ Calculator v.1.0 .py
@@ -1,20 +1,30 @@
 print("$" * 10, "Калькулятор v 1.0", "$" * 10)
+
 while True:
+
     print("Введите q для выхода")
     s = input("Знак (+,-,*,/): ")
+
     if s == 'q': break
+
     if s in ('+', '-', '*', '/'):
         x = float(input("Введите x: "))
         y = float(input("Введите y: "))
+
     if s == "+":
         print("%.2f" % (x + y))
+
     elif s == "-":
         print("%.2f" % (x - y))
+
     elif s == "*":
         print("%.2f" % (x * y))
+
     elif s == "/":
+
         if y != 0:
             print("%.2f" % (x / y))
+
         else:
             print("Деление на ноль")
     else:
