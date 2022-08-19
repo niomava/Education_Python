299323dfb553de91d3d67a5dca5b8934c9d58bb0
AVRybin <52917948+AVRybin@users.noreply.github.com>
2022-08-19 20:45:11 +0600

    Code Style Example

--- Calculator v.1.0 .py
+++ Calculator v.1.0 .py
@@ -3,27 +3,25 @@ print("$" * 10, "Калькулятор v 1.0", "$" * 10)
 while True:
     print("Введите q для выхода")
     s = input("Знак (+,-,*,/): ")
+    
     if s == 'q': break
 
     if s not in ('+', '-', '*', '/'):
             print("неверный знак")
             continue
+            
     x = float(input("Введите x: "))
     y = float(input("Введите y: "))
 
     if s == "+":
         print("%.2f" % (x + y))
-
     elif s == "-":
         print("%.2f" % (x - y))
-
     elif s == "*":
         print("%.2f" % (x * y))
-
     elif s == "/":
-
+        
         if y != 0:
             print("%.2f" % (x / y))
-
         else:
             print("Деление на ноль")
